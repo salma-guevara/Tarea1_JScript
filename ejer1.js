6f1231a4f09529a77b39eec3bf3fceef0eb67be7
Salma Guevara <ada_hack421@hackademy.mx>
2022-01-28 15:08:31 -0600

    Borré visualización  de no primos

--- ejer1.js
+++ ejer1.js
@@ -4,7 +4,7 @@ for (var i = 0 ; i < ejercicio1.length; i++){
 }
 console.log(ejercicio1);
 */
-console.log("Ejercicio 1: Números primos: ")
+console.log("Ejercicio 1: Números primos. ")
 const ejercicio1 = [3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29, 19, 88, 456, 13, 23, 24];
 // Primero entra aquí y luego llama a la función
 for (let indice = 0 ; indice < ejercicio1.length ; indice++){
@@ -13,7 +13,7 @@ for (let indice = 0 ; indice < ejercicio1.length ; indice++){
     if(choose){
         console.log(`${ejercicio1[indice]} ES primo`)
     }else{
-        console.log(`${ejercicio1[indice]} NO es primo`)
+        //console.log(`${ejercicio1[indice]} NO es primo`)
     }
 
 }
